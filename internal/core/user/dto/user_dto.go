79245eabe9d62f921feaefd79b266e870b4be5bb
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-11 15:46:54 +0700

    add loginresponse

--- internal/core/user/dto/user_dto.go
+++ internal/core/user/dto/user_dto.go
@@ -1,1 +1,20 @@
 package dto
+
+import "time"
+
+type RegisterRequest struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+type LoginRequest struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+type UserResponse struct {
+	Name      string    `json:"Name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type LoginResponse struct {
+	AccessToken string `json: "access_token"`
+}
