9ade66e012bab32b240adabce1b4be77b78ab9a1
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-12 17:19:46 +0700

    Implementasi login handler and setcookie token

--- internal/core/user/controller/user_controllerImp.go
+++ internal/core/user/controller/user_controllerImp.go
@@ -56,3 +56,58 @@ func (controller *UserControllerImp) Register(writer http.ResponseWriter, reques
 		Data:   user,
 	})
 }
+
+func (controller *UserControllerImp) Login(writer http.ResponseWriter, request *http.Request) {
+
+	ctx := request.Context()
+
+	var input dto.LoginRequest
+	if err := json.NewDecoder(request.Body).Decode(&input); err != nil {
+		writer.Header().Set("content-type", "application/json")
+		writer.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(writer).Encode(dto.Response{
+			Code:   http.StatusBadRequest,
+			Status: "Bad Request",
+			Data:   "input salah",
+		})
+		return
+	}
+	userResponse, err := controller.authservice.Login(ctx, input)
+	if err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		//errorcredensial
+		if err == service.ErrInvalidCredential {
+			writer.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(writer).Encode(dto.Response{
+				Code:   http.StatusUnauthorized,
+				Status: "unauthorize",
+				Data:   "Username or password failed",
+			})
+			return
+		}
+		writer.Header().Set("content-type", "application/json")
+		writer.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(writer).Encode(dto.Response{
+			Code:   http.StatusInternalServerError,
+			Status: "500",
+			Data:   "internal server error",
+		})
+		return
+	}
+	http.SetCookie(writer, &http.Cookie{
+		Name:     "token",
+		Value:    userResponse.AccessToken,
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   false,
+		MaxAge:   900,
+	})
+	writer.Header().Set("content-type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	json.NewEncoder(writer).Encode(dto.Response{
+		Code:   http.StatusCreated,
+		Status: "created",
+		Data:   userResponse.User,
+	})
+
+}
