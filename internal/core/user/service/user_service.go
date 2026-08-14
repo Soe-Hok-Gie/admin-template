d8b87bf3be9c77a4d0af1a041769dfdd90154427
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-14 16:58:46 +0700

    create kontrak getProfile

--- internal/core/user/service/user_service.go
+++ internal/core/user/service/user_service.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"admin-template/internal/core/user/domain"
+	"admin-template/internal/core/user/dto"
+	"context"
+)
+
+type UserService interface {
+	GetProfile(ctx context.Context, userID int64) (domain.User, dto.UserResponse, error)
+}
