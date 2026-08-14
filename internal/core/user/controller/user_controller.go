81fe0940298f288e90a33438821156c9838ccb31
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-14 16:54:54 +0700

    create kontrak profile

--- internal/core/user/controller/user_controller.go
+++ internal/core/user/controller/user_controller.go
@@ -5,4 +5,5 @@ import "net/http"
 type UserController interface {
 	Register(writer http.ResponseWriter, request *http.Request)
 	Login(writer http.ResponseWriter, request *http.Request)
+	Profile(writer http.ResponseWriter, request *http.Request)
 }
