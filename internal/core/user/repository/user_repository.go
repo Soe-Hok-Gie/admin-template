114181473fcbd0884f40f4490cc6e60fddcfeca5
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-17 20:47:42 +0700

    create interface GetById

--- internal/core/user/repository/user_repository.go
+++ internal/core/user/repository/user_repository.go
@@ -10,4 +10,5 @@ import (
 type UserRepository interface {
 	Insert(ctx context.Context, user domain.User) (domain.User, error)
 	GetByEmail(ctx context.Context, email string) (domain.User, error)
+	GedById(ctx context.Context, userID int64) (domain.User, error)
 }
