e7df4f276bb250d528fe6b602c2f22ba62e1b59a
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-17 21:42:36 +0700

    injek func polimorphic

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"admin-template/internal/fondation"
+	"fmt"
 )
 
 func main() {
 	cfg := fondation.Load()
 
-	fmt.Println("Application :", cfg.App.Name)
-	fmt.Println("Environment :", cfg.App.Env)
-	fmt.Println("Port        :", cfg.App.Port)
+	// fmt.Println("Application :", cfg.App.Name)
+	// fmt.Println("Environment :", cfg.App.Env)
+	// fmt.Println("Port        :", cfg.App.Port)
 
-	fmt.Println()
+	// fmt.Println()
 
-	fmt.Println("Database Host :", cfg.DB.Host)
-	fmt.Println("Database Port :", cfg.DB.Port)
-	fmt.Println("Database User :", cfg.DB.User)
-	fmt.Println("Database Name :", cfg.DB.Name)
+	// fmt.Println("Database Host :", cfg.DB.Host)
+	// fmt.Println("Database Port :", cfg.DB.Port)
+	// fmt.Println("Database User :", cfg.DB.User)
+	// fmt.Println("Database Name :", cfg.DB.Name)
 
 	db := fondation.NewDB(cfg.DB)
+
 	defer db.Close()
 
 	fmt.Println("Database connected")
