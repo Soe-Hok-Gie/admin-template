2925969f4e964114dcbfa3dcc5ca3b32b24f2c98
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-20 17:15:49 +0700

    Done main

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"admin-template/internal/core/user/controller"
+	"admin-template/internal/core/user/middleware"
+	"admin-template/internal/core/user/repository"
+	"admin-template/internal/core/user/service"
 	"admin-template/internal/fondation"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	cfg := fondation.Load()
-
-	// fmt.Println("Application :", cfg.App.Name)
-	// fmt.Println("Environment :", cfg.App.Env)
-	// fmt.Println("Port        :", cfg.App.Port)
-
-	// fmt.Println()
-
-	// fmt.Println("Database Host :", cfg.DB.Host)
-	// fmt.Println("Database Port :", cfg.DB.Port)
-	// fmt.Println("Database User :", cfg.DB.User)
-	// fmt.Println("Database Name :", cfg.DB.Name)
-
 	db := fondation.NewDB(cfg.DB)
-
 	defer db.Close()
 
+	userRepository := repository.NewUserRepository(db)
+	authService := service.NewAuthService(userRepository)
+	userService := service.NewUserService(userRepository)
+	userController := controller.NewUserController(authService, userService)
+
+	r := mux.NewRouter()
+	//rute auth
+	r.HandleFunc("/auth/register", userController.Register).Methods("POST")
+	r.HandleFunc("/auth/login", userController.Login).Methods("POST")
+
+	//rute user, harus proteksi dengan middleware
+	jwtMiddleware := middleware.JWTMiddleware()
+	r.Handle("/auth/login/profile", jwtMiddleware(http.HandlerFunc(userController.Profile))).Methods("GET")
+
 	fmt.Println("Database connected")
+	// Menggabungkan tanda ":" dengan port aplikasi (8080) dari .env
+	serverAddress := ":" + cfg.App.Port
+	fmt.Println("Server Go berjalan di port", serverAddress)
+	log.Fatal(http.ListenAndServe(serverAddress, r))
+
 }
