51ebbd2eb8ebfa263835f00f64317188eb7ce205
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-24 21:01:25 +0700

    ambil userID dari middleware

--- internal/core/user/controller/order_controllerImp.go
+++ internal/core/user/controller/order_controllerImp.go
@@ -1,10 +1,29 @@
 package controller
 
-import "net/http"
+import (
+	"admin-template/internal/core/user/dto"
+	"admin-template/internal/core/user/middleware"
+	"encoding/json"
+	"net/http"
+)
 
 type OrderControllerImp struct {
 }
 
 func (controller *OrderControllerImp) Checkout(writer http.ResponseWriter, request *http.Request) {
+
+	ctx := request.Context()
+	//panggil userId
+	userID, ok := request.Context().Value(middleware.UserIDKey)(int64)
+	if !ok {
+		writer.Header().Set("content-type", "aplication/json")
+		writer.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(writer).Encode(dto.Response{
+			Code:   http.StatusUnauthorized,
+			Status: "Unauthorized",
+			Data:   nil,
+		})
+		return
+	}
 
 }
