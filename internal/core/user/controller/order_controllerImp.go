80b1f3e742be00f3e434b265d3351f0219cbaf0a
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-08-24 21:40:21 +0700

    panggil order service

--- internal/core/user/controller/order_controllerImp.go
+++ internal/core/user/controller/order_controllerImp.go
@@ -14,7 +14,7 @@ func (controller *OrderControllerImp) Checkout(writer http.ResponseWriter, reque
 
 	ctx := request.Context()
 	//panggil userId
-	userID, ok := request.Context().Value(middleware.UserIDKey)(int64)
+	userID, ok := request.Context().Value(middleware.UserIDKey).(int64)
 	if !ok {
 		writer.Header().Set("content-type", "aplication/json")
 		writer.WriteHeader(http.StatusUnauthorized)
@@ -25,5 +25,32 @@ func (controller *OrderControllerImp) Checkout(writer http.ResponseWriter, reque
 		})
 		return
 	}
+
+	response, err := controller.orderService.CreateOrder(ctx, userID)
+	if err != nil {
+		writer.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(writer).Encode(dto.Response{
+			Code:   http.StatusUnauthorized,
+			Status: "Unauthorized",
+			Data:   nil,
+		})
+		return
+	}
+	writer.Header().Set("content-type", "application/json")
+	writer.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(writer).Encode(dto.Response{
+		Code:   http.StatusInternalServerError,
+		Status: "500",
+		Data:   "server error",
+	})
+	return
+
+	writer.Header().Set("content-type", "application")
+	writer.WriteHeader(http.StatusCreated)
+	json.NewEncoder(writer).Encode(dto.Response{
+		Code:   http.StatusCreated,
+		Status: "create",
+		Data:   response,
+	})
 
 }
