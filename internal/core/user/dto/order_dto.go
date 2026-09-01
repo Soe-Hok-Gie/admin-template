acd9ad4316b62c286fec39529246b3970cb4a511
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-09-01 10:48:38 +0700

    change data type,Akurasi Signature: Menghindari salah perhitungan hash SHA512 akibat hilangnya angka desimal .00 yang dipersyaratkan oleh Midtrans.

--- internal/core/user/dto/order_dto.go
+++ internal/core/user/dto/order_dto.go
@@ -18,5 +18,5 @@ type WebhookNotification struct {
 	OrderID           string `json:"order_id"`
 	StatusCode        string `json:"status_code"`
 	SignatureKey      string `json:"signature_key"`
-	GrossAmount       int64  `json:"gross_amount"`
+	GrossAmount       string `json:"gross_amount"`
 }
